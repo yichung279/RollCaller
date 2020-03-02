61c800b2389363f4d85b9426c864db511f6115fb
yiz <ql3895619@gmail.com>
2020-03-02 16:02:16 +0800

    Feat: add scanner template

---
+++ app/AppScanner.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+#app.container
+</template>
+
+<script>
+import 'semantic-ui-offline/semantic.min.css'
+
+export default {
+}
+</script>
+
+<style lang="sass">
+body,#app
+  height: 100%
+  overflow: hidden
+</style>
+
+<style lang="sass" scoped>
+.container
+  padding: 5vh
+  display: flex
+  align-items: center
+  flex-direction: column
+
+</style>
+
+<!--
+  vi:et:sw=2:ts=2
+-->
